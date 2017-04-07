853888efa78596fc84e1296868316d2ccd18a514
justinlarkin <justinlarkin@10.182.185.105>
2017-04-06 22:44:41 -0400

    Partial implementation of displaying the numbers.

--- GitLab/src/SalesData.java
+++ GitLab/src/SalesData.java
@@ -1,11 +1,13 @@
 
 public class SalesData {
-   int data[] = {0, 4, 42};
+   int data[] = {0, 4, 21};
 
    public void display() {
-       System.out.println("TODO: Implement data display");
+	   for(int i = 0; i < data.length;i++) {
+		   System.out.println("");
+	   }
    } //End display
 
-}  //End class SalesData
+}  //End class SalesData 
 
   
